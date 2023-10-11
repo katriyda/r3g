0bdd108c3e54d03d9ad953473e6072cb507102d5
katriyda <63391334+katriyda@users.noreply.github.com>
2023-10-12 00:16:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,13 +10,13 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 # Hello World
-# rm -rf package/helloworld
-# git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
-# git -C package/helloworld pull
+rm -rf package/helloworld
+git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+git -C package/helloworld pull
 
 # wechatpush
-# rm -rf package/wechatpush
-# git clone https://github.com/tty228/luci-app-wechatpush.git package/wechatpush
+rm -rf package/wechatpush
+git clone https://github.com/tty228/luci-app-wechatpush.git package/wechatpush
 
 # mosdns
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
@@ -25,8 +25,18 @@ git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 # argon
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+# 删除自定义源默认的 argon 主题
+rm -rf package/lean/luci-theme-argon
+
+# 部分第三方源自带 argon 主题，上面命令删除不掉的请运行下面命令
+find ./ -name luci-theme-argon | xargs rm -rf;
+
+# 针对 LEDE 项目拉取 argon 原作者的源码
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+
+# 替换默认主题为 luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 
 # golang update
-# rm -rf feeds/packages/lang/golang
-# git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
