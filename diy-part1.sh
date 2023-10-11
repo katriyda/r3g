4e35211af1671c84d7e23b38c2932f3dbed581c5
katriyda <63391334+katriyda@users.noreply.github.com>
2023-10-11 13:35:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,13 +10,13 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 # Hello World
-rm -rf package/helloworld
-git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
-git -C package/helloworld pull
+# rm -rf package/helloworld
+# git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+# git -C package/helloworld pull
 
 # wechatpush
-rm -rf package/wechatpush
-git clone https://github.com/tty228/luci-app-wechatpush.git package/wechatpush
+# rm -rf package/wechatpush
+# git clone https://github.com/tty228/luci-app-wechatpush.git package/wechatpush
 
 # mosdns
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
@@ -25,5 +25,5 @@ git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 # golang update
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
+# rm -rf feeds/packages/lang/golang
+# git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
