3a7903e2b86cfa13ad633e08ea06bddd02ffe3ff
katriyda <63391334+katriyda@users.noreply.github.com>
2023-10-12 17:58:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,15 +24,6 @@ find ./ | grep Makefile | grep mosdns | xargs rm -f
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
-# argon
-# 删除自定义源默认的 argon 主题
-rm -rf package/lean/luci-theme-argon
-
-# 部分第三方源自带 argon 主题，上面命令删除不掉的请运行下面命令
-find ./ -name luci-theme-argon | xargs rm -rf;
-
-# 针对 LEDE 项目拉取 argon 原作者的源码
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
 # golang update
 rm -rf feeds/packages/lang/golang
